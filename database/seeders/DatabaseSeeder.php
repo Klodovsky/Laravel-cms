730535ae5866c79f35edc87460f42553f5cb2d0f
Klodovskyy <55706752+Klodovskyy@users.noreply.github.com>
2021-05-01 21:49:56 -0400

    added database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        \DB::table('users')->truncate();
+        \DB::table('posts')->truncate();
+
+        //creating 10 dummy users with 2 posts each
+        User::factory(10)->has(Post::factory()->count(2))->create();
+        //10 creating dummy posts
+        Post::factory(10)->create();
+
+
+        User::factory(10)->create()->each(function($user){
+        $user->post()->save(Post::factory()->make());
+        });
     }
 }
